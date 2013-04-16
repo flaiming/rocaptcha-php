e28b3777c24a9ad2f1af12f9577e768138a997dd
Vojtěch Oram <vojtech@oram.cz>
2013-04-16 21:27:54 +0200

    Fixed comments.

--- rocaptcha-example.php
+++ rocaptcha-example.php
@@ -8,12 +8,12 @@ require_once('rocaptchalib.php');
 $publickey = "49c47baecc3738a36bb99665e290fc";
 $privatekey = "e5b725f71537ec50ceee5c6a9b27d6";
 
-# the response from reCAPTCHA
+# the response from RoCATPCHA
 $resp = null;
-# the error code from reCAPTCHA, if any
+# the error code from RoCATPCHA, if any
 $error = null;
 
-# was there a reCAPTCHA response?
+# was there a RoCATPCHA response?
 if (isset($_POST["rocaptcha_response_field"])) {
         $resp = RoCaptcha::checkAnswer($privatekey,
                                         $_SERVER["REMOTE_ADDR"],
@@ -21,16 +21,16 @@ if (isset($_POST["rocaptcha_response_field"])) {
                                         $_POST["rocaptcha_response_field"]);
 
         if ($resp->is_valid) {
-                echo "You got it!";
+                echo "Passed!";
         } else {
                 # set the error code so that we can display it
                 $error = $resp->error;
-				echo "WRONG";
+				echo "Please try again.";
         }
 }
 echo RoCaptcha::getHtml($publickey, $error, 'cs');
 ?>
-    <br/>
+    <br />
     <input type="submit" value="submit" />
     </form>
   </body>
